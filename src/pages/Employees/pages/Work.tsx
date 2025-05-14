6a77821677111126a878535e1f1dd060a4c97736
Millborne <millborne.galamiton@supportzebra.com>
2025-05-14 14:50:29 +0800

    Update Work.tsx

--- src/pages/Employees/pages/Work.tsx
+++ src/pages/Employees/pages/Work.tsx
@@ -1,5 +1,36 @@
+import { Tab } from "enterprisze-global-components";
+import { useState } from "react";
+
+const tabs = ["Employee Details", "Team Members", "Employment History"];
+
 const Work = () => {
-    return <div>Work</div>;
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+
+    return (
+        <div className="flex flex-col gap-[24px] p-4">
+            <div className="flex">
+                <Tab
+                    label="Employee Details"
+                    type="left"
+                    isFirst
+                    active={activeTab === "Employee Details"}
+                    onClick={() => setActiveTab("Employee Details")}
+                />
+                <Tab
+                    label="Team Members"
+                    type="middle"
+                    active={activeTab === "Team Members"}
+                    onClick={() => setActiveTab("Team Members")}
+                />
+                <Tab
+                    label="Employment History"
+                    type="right"
+                    active={activeTab === "Employment History"}
+                    onClick={() => setActiveTab("Employment History")}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Work;
