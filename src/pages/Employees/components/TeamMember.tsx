b23829531dca72bc7559016a5a0c2c8b0e032ab1
Millborne Galamiton <150405245+Millborne@users.noreply.github.com>
2025-05-20 16:24:46 +0800

    hotfix/employee work (#22)
    * Added react flow
    
    * Update TeamMember.tsx

--- src/pages/Employees/components/TeamMember.tsx
+++ src/pages/Employees/components/TeamMember.tsx
@@ -1,173 +1,207 @@
-import { CardContainer, Table } from "enterprisze-global-components";
+import { CardContainer } from "enterprisze-global-components";
 import { Edit2 } from "iconsax-reactjs";
 import papaZEager from "../../../assets/papaz-eager.png";
 
+import { ReactFlow } from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+    { id: "1", data: { label: "SPT Supervisor 2" } },
+    {
+        id: "2",
+        data: { label: "Junior Web Developer 2" },
+        style: {
+            background: "#FF9900",
+            border: "1px solid #CC7A00",
+            borderRadius: "8px",
+            padding: "10px",
+            color: "#FFFFFF",
+        },
+    },
+    {
+        id: "3",
+        data: { label: "Junior Web Developer 3" },
+    },
+].map((node, index) => ({
+    ...node,
+    // Calculate positions dynamically based on index
+    position: {
+        x: 400, // Center x position
+        y: 200 + index * 100, // Stack vertically with 100px spacing
+    },
+}));
+
+const initialEdges = [
+    { id: "e1-2", source: "1", target: initialNodes.length.toLocaleString() },
+];
+
 const TeamMember = () => {
-    const showData = false;
-    const headers: Array<
-        | { type: "string"; header: string; accessor: string }
-        | { type: "more"; header: React.ReactNode; accessor: "more" }
-        | { type: "checkbox"; header: React.ReactNode; accessor: "checkbox" }
-    > = [
-        { type: "string", header: "Name", accessor: "name" },
-        { type: "string", header: "ID", accessor: "id" },
-        { type: "string", header: "Team", accessor: "team" },
-        { type: "string", header: "Job Title", accessor: "jobTitle" },
-        { type: "string", header: "Job Code", accessor: "jobCode" },
-        { type: "string", header: "Direct Head", accessor: "directHead" },
-        { type: "more", header: <></>, accessor: "more" },
-    ];
+    const showData = true;
+    // const headers: Array<
+    //     | { type: "string"; header: string; accessor: string }
+    //     | { type: "more"; header: React.ReactNode; accessor: "more" }
+    //     | { type: "checkbox"; header: React.ReactNode; accessor: "checkbox" }
+    // > = [
+    //     { type: "string", header: "Name", accessor: "name" },
+    //     { type: "string", header: "ID", accessor: "id" },
+    //     { type: "string", header: "Team", accessor: "team" },
+    //     { type: "string", header: "Job Title", accessor: "jobTitle" },
+    //     { type: "string", header: "Job Code", accessor: "jobCode" },
+    //     { type: "string", header: "Direct Head", accessor: "directHead" },
+    //     { type: "more", header: <></>, accessor: "more" },
+    // ];
 
-    const data = [
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-        {
-            id: "1234567890",
-            name: (
-                <div className=" flex items-center gap-1">
-                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
-                    <span className="text-body-base-reg">
-                        Germanotta, Stephanie Luke A.
-                    </span>
-                </div>
-            ),
-            team: "BSI",
-            jobTitle: "Junior Web Developer",
-            jobCode: "1234567890",
-            directHead: "John Doe",
-        },
-    ];
+    // const data = [
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    //     {
+    //         id: "1234567890",
+    //         name: (
+    //             <div className=" flex items-center gap-1">
+    //                 <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+    //                 <span className="text-body-base-reg">
+    //                     Germanotta, Stephanie Luke A.
+    //                 </span>
+    //             </div>
+    //         ),
+    //         team: "BSI",
+    //         jobTitle: "Junior Web Developer",
+    //         jobCode: "1234567890",
+    //         directHead: "John Doe",
+    //     },
+    // ];
 
-    const moreOptions = [
-        {
-            label: "View",
-            onClick: (index: number) => {
-                console.log("navigate to row:", index);
-            },
-        },
-        {
-            label: "Delete",
-            onClick: (index: number) => console.log("Delete row:", index),
-        },
-    ];
+    // const moreOptions = [
+    //     {
+    //         label: "View",
+    //         onClick: (index: number) => {
+    //             console.log("navigate to row:", index);
+    //         },
+    //     },
+    //     {
+    //         label: "Delete",
+    //         onClick: (index: number) => console.log("Delete row:", index),
+    //     },
+    // ];
 
     return (
         <CardContainer
@@ -181,20 +215,44 @@ const TeamMember = () => {
                         <Edit2 className="icon-sm text-szPrimary900" />
                     </div>
                     <div
-                        className={`${
+                        className={`h-[500px] ${
                             showData
                                 ? "overflow-x-auto"
-                                : "flex justify-center items-center h-[400px]"
+                                : "flex justify-center items-center "
                         } `}
                     >
                         {showData ? (
                             <>
-                                <div className=" h-[400px] overflow-y-auto">
-                                    <Table
-                                        headers={headers}
-                                        data={data}
-                                        moreOptions={moreOptions}
-                                    />
+                                <div className="h-full overflow-y-auto">
+                                    <div className="bg-[#F9F9F9] h-full rounded-[16px] font-montserrat">
+                                        <ReactFlow
+                                            className="bg-[#F9F9F9] h-full rounded-[16px] font-montserrat"
+                                            nodes={initialNodes.map(
+                                                (node) => ({
+                                                    ...node,
+                                                    style: !node.style
+                                                        ? {
+                                                              background:
+                                                                  "#FFEFD6",
+                                                              border: "1px solid #FFDEAD",
+                                                              borderRadius:
+                                                                  "8px",
+                                                              padding: "10px",
+                                                          }
+                                                        : node.style,
+                                                })
+                                            )}
+                                            edges={initialEdges}
+                                            defaultEdgeOptions={{
+                                                style: {
+                                                    stroke: "#FFDEAD",
+                                                    strokeWidth: 2,
+                                                },
+                                                type: "default",
+                                                animated: false,
+                                            }}
+                                        />
+                                    </div>
                                 </div>
                             </>
                         ) : (
