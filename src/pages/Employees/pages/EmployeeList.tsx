9ad4ee012319a241826878324397c913a81b206a
Millborne <millborne.galamiton@supportzebra.com>
2025-05-21 11:53:32 +0800

    style: add more mock data and legends to table header

--- src/pages/Employees/pages/EmployeeList.tsx
+++ src/pages/Employees/pages/EmployeeList.tsx
@@ -8,7 +8,12 @@ import {
     Pagination,
     Table,
 } from "enterprisze-global-components";
-import { ArrowDown2, ArrowUp2, SearchNormal } from "iconsax-reactjs";
+import {
+    ArrowDown2,
+    ArrowUp2,
+    InfoCircle,
+    SearchNormal,
+} from "iconsax-reactjs";
 
 const EmployeeList = () => {
     const navigate = useNavigate();
@@ -19,12 +24,57 @@ const EmployeeList = () => {
 
     // For larger screen
     const headers: Array<
-        | { type: "string"; header: string; accessor: string }
+        | {
+              type: "string";
+              header: string;
+              accessor: string;
+              icon?: React.ReactNode;
+          }
         | { type: "more"; header: React.ReactNode; accessor: "more" }
         | { type: "checkbox"; header: React.ReactNode; accessor: "checkbox" }
     > = [
         { type: "checkbox", header: <></>, accessor: "checkbox" },
-        { type: "string", header: "Name", accessor: "name" },
+        {
+            type: "string",
+            header: "Name",
+            accessor: "name",
+            icon: (
+                <div className="relative group">
+                    <InfoCircle className="w-4 h-4 text-szBlack700 hover:text-szPrimary700 transition-colors duration-200 cursor-help" />
+                    <div className="absolute z-10 invisible group-hover:visible bg-white shadow-lg rounded-lg p-2 w-[97px] -left-20 top-6">
+                        <div className="flex flex-col gap-2 w-full items-start">
+                            <span className="text-body-small-reg text-szBlack800">
+                                Legends:
+                            </span>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-success700 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Active
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-szGrey300 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Inactive
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-info500 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Floating
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-warning500 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Clearance
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ),
+        },
         { type: "string", header: "ID", accessor: "id" },
         { type: "string", header: "Team", accessor: "team" },
         { type: "string", header: "Job Title", accessor: "jobTitle" },
@@ -36,10 +86,9 @@ const EmployeeList = () => {
     const data = [
         {
             id: "1234567890",
-            title: "A Back to the Future",
             name: (
-                <div className=" flex items-center gap-1">
-                    <div className="hidden lg:!block w-[14px] h-[14px] rounded-full bg-success700"></div>
+                <div className="md:flex items-center gap-1">
+                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
                     <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
                         Germanotta, Stephanie Luke A.
                     </span>
@@ -58,15 +107,129 @@ const EmployeeList = () => {
             jobCode: "1234567890",
             directHead: "John Doe",
         },
+        {
+            id: "2345678901",
+            name: (
+                <div className="md:flex items-center gap-1">
+                    <div className="w-[14px] h-[14px] rounded-full bg-success700"></div>
+                    <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                        Smith, John William B.
+                    </span>
+                </div>
+            ),
+            team: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Shoopee
+                </span>
+            ),
+            jobTitle: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Customer Service Representative
+                </span>
+            ),
+            jobCode: "2345678901",
+            directHead: "Jane Smith",
+        },
+        {
+            id: "3456789012",
+            name: (
+                <div className="md:flex items-center gap-1">
+                    <div className="w-[14px] h-[14px] rounded-full bg-info500"></div>
+                    <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                        Johnson, Emily Rose C.
+                    </span>
+                </div>
+            ),
+            team: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Shoopee
+                </span>
+            ),
+            jobTitle: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Customer Service Representative
+                </span>
+            ),
+            jobCode: "3456789012",
+            directHead: "Michael Brown",
+        },
+        {
+            id: "4567890123",
+            name: (
+                <div className="md:flex items-center gap-1">
+                    <div className="w-[14px] h-[14px] rounded-full bg-warning500"></div>
+                    <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                        Davis, Robert James D.
+                    </span>
+                </div>
+            ),
+            team: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Shoopee
+                </span>
+            ),
+            jobTitle: (
+                <span className="text-body-base-reg lg:truncate max-w-[120px] lg:max-w-none block">
+                    Manager
+                </span>
+            ),
+            jobCode: "4567890123",
+            directHead: "Sarah Wilson",
+        },
     ];
 
     // For smaller screen
     const headersSmall: Array<
-        | { type: "string"; header: string; accessor: string }
+        | {
+              type: "string";
+              header: string;
+              accessor: string;
+              icon?: React.ReactNode;
+          }
         | { type: "more"; header: React.ReactNode; accessor: "more" }
         | { type: "checkbox"; header: React.ReactNode; accessor: "checkbox" }
     > = [
-        { type: "string", header: "Name", accessor: "name" },
+        {
+            type: "string",
+            header: "Name",
+            accessor: "name",
+            icon: (
+                <div className="relative group">
+                    <InfoCircle className="w-4 h-4 text-szBlack700 hover:text-szPrimary700 transition-colors duration-200 cursor-help" />
+                    <div className="absolute z-10 invisible group-hover:visible bg-white shadow-lg rounded-lg p-2 w-[97px] -left-20 top-6">
+                        <div className="flex flex-col gap-2 w-full items-start">
+                            <span className="text-body-small-reg text-szBlack800">
+                                Legends:
+                            </span>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-success700 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Active
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-szGrey300 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Inactive
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-info500 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Floating
+                                </span>
+                            </div>
+                            <div className="flex items-center gap-[10px]">
+                                <div className="w-[14px] h-[14px] bg-warning500 rounded-full"></div>
+                                <span className="text-caption-reg text-szBlack800">
+                                    Clearance
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ),
+        },
         { type: "string", header: "Team", accessor: "team" },
         { type: "string", header: "Job Title", accessor: "jobTitle" },
         { type: "more", header: <></>, accessor: "more" },
