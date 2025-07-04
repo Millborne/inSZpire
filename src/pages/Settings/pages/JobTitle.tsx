c01aa7c815d8b7ccc804c30f2b6be910128d9517
Kara Ladera <kara.ladera@supportzebra.com>
2025-07-04 15:21:50 +0800

    add sidebar menu on settings

---
+++ src/pages/Settings/pages/JobTitle.tsx
@@ -0,0 +1,12 @@
+import { HamburgerMenu } from "iconsax-reactjs";
+
+const JobTitle = () => {
+    return (
+        <div>
+            <HamburgerMenu />
+            JobTitle
+        </div>
+    );
+};
+
+export default JobTitle;
