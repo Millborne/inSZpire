5de842edd85a7df0767b5183ec3f2b6947c2ce26
Millborne <millborne.galamiton@supportzebra.com>
2025-07-08 15:35:25 +0800

    fix: Update team service

---
+++ src/services/teams/index.ts
@@ -0,0 +1,14 @@
+export {
+    teamsAPI,
+    useFetchTeamsQuery,
+    useActionTeamsMutation,
+} from "./teamsAPI";
+export {
+    useTeams,
+    useTeamService,
+    type TeamData,
+    type CreateTeamRequest,
+    type UpdateTeamRequest,
+    type ViewTeamsRequest,
+    type BatchUpdateStatusRequest,
+} from "./use-teams";
